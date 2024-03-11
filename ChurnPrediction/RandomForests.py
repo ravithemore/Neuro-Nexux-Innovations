76d8e560b6646e19446fad0a549810f19bf301a5
Ravi M Damor <damorravi540@gmail.com>
2024-03-12 01:19:46 +0530

    Update RandomForests.py

--- ChurnPrediction/RandomForests.py
+++ ChurnPrediction/RandomForests.py
@@ -1,5 +1,4 @@
-#SIDDHARTH GUPTA, FOR NEURONEXUS INNOVATIONS
-
+#Ravi M Damor ,NeuroNexus Project
 # Here we import all the required libraries for our logistic regression model..
 import pandas as pd #for reading the dataset
 from sklearn.model_selection import train_test_split #to train and test our data pieces..
